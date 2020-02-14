3a4eb1099aedad81cee181b6492728927388b340
丘天惠 <43975076+Autumnhui@users.noreply.github.com>
2020-02-14 21:40:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Draw-TextPic
+ 通过上传图片，利用pillow模块输出以文字覆盖的图片
---
+++ main.py
@@ -0,0 +1,26 @@
+# Author: Autumnhui
+from PIL import Image, ImageDraw, ImageFont
+
+font_size = 3   # 字体大小
+text = "文字"   #文字内容
+img_path = "/Users/autumnhui/Desktop/death.jpeg"  # 图片来源
+
+img_raw = Image.open(img_path)
+img_array = img_raw.load()
+
+img_new = Image.new("RGB", img_raw.size, (0,0,0)) # rgb色彩代码，默认覆盖为黑色
+draw = ImageDraw.Draw(img_new)
+font = ImageFont.truetype('/Library/Fonts/STHeiti Medium.ttc', font_size) # 字体路径
+
+def character_generator(text):
+    while True:
+        for i in range(len(text)):
+            yield text[i]
+
+ch_gen = character_generator(text)
+
+for y in range(0, img_raw.size[1], font_size):
+    for x in range(0, img_raw.size[0], font_size):
+        draw.text((x, y), next(ch_gen), font=font, fill=img_array[x, y], direction=None)
+
+img_new.convert('RGB').save("/Users/autumnhui/Desktop/a.jpeg") # 文件导出（具体到文件）
